5b59d04b77a870d208f8c49032596918094ed62e
Vikram <vikram.maiya@jodovatech.com>
2018-02-13 15:09:53 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@
 var socket =io();
 socket.on('connect', function(){
   console.log('Connected to Server');
-  socket.emit('createMessage',{
-    from: 'mala',
-    text: 'Hey this is mala'
-  });
 });
 socket.on('disconnect', function(){
   console.log('Disconnected from Server');
--- server/server.js
+++ server/server.js
@@ -11,10 +11,11 @@ const io = socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
-  socket.emit('newMessage',{from: 'Vikram', text: 'Hey, what is going on', createdAt: 123});
+
 
   socket.on('createMessage',(message)=>{
     console.log('createMessage', message);
+    io.emit('newMessage',{from: message.from, text: message.text, createdAt: new Date().getTime()});
   });
   socket.on('disconnect',() => {
       console.log('User was disconnected');
