f06f543a63df99827ea5c531e5ae1eaadf07600e
Vikram <vikram.maiya@jodovatech.com>
2018-02-15 12:46:12 +0530

    add users class and updateuser event

---
+++ server/utils/validation.test.js
@@ -0,0 +1,20 @@
+const expect = require('expect');
+const {isRealString} = require('./validation');
+//should reject non string values
+//should reject string with only spaces
+// should allow string with non-space characters
+
+describe('isRealString',() => {
+  it('should reject non string values',() => {
+    var res = isRealString(98);
+    expect(res).toBe(false);
+  });
+  it('should reject string with only spaces',()=> {
+    var res = isRealString('   ');
+    expect(res).toBe(false);
+  });
+  it('should allow string with non-space characters',() => {
+    var res = isRealString('  vikram  ');
+    expect(res).toBe(true);
+  });
+});
